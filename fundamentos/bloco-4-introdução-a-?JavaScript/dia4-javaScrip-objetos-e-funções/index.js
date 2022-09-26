064e6fd575f70f79b8f1c1c9cfc69beaee494bc7
Adriana Chikuji <drikasousa92@gmail.com>
2022-09-26 19:15:27 -0300

    ptimeiro exercicio completo

--- fundamentos/bloco-4-introdução-a-?JavaScript/dia4-javaScrip-objetos-e-funções/index.js
+++ fundamentos/bloco-4-introdução-a-?JavaScript/dia4-javaScrip-objetos-e-funções/index.js
@@ -19,3 +19,21 @@ for (let chave in info){
 for (let chave in info){
     console.log(info[chave]);
 }
+//--------------------------------------------------------------------------------
+console.log('\n\n--------------')
+let info2= {
+    personagem: 'Tio Patinhas',
+    origem: 'Christmas on Bear Mountain, Dell\'s Four Color Comics #178',
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim'
+  };
+
+  console.log(info.personagem + ' e ' + info2.personagem);
+  console.log(info.origem+ ' e ' + info2.origem);
+  console.log(info.nota + ' e ' + info2.nota);
+  if (info.recorrente === info2.recorrente && info.recorrente === 'Sim'){
+    console.log('Ambos recorrentes')
+  } else{
+    console.log('Ambos não recorrentes')
+  }
+  
