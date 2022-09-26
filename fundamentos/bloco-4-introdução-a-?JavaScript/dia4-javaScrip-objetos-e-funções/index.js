b237262a11bb88529424bea3204b55001e0924a9
Adriana Chikuji <drikasousa92@gmail.com>
2022-09-26 18:55:08 -0300

    segunda parte primeiro exercicio

--- fundamentos/bloco-4-introdução-a-?JavaScript/dia4-javaScrip-objetos-e-funções/index.js
+++ fundamentos/bloco-4-introdução-a-?JavaScript/dia4-javaScrip-objetos-e-funções/index.js
@@ -4,7 +4,18 @@ let info = {
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   };
 const mensagem = 'Bem vinda, '
-console.log('Bem vinda, ' + info.personagem);
-console.log(`Bem vinda, ${info.personagem}`);
-console.log(mensagem + info.personagem);
+//console.log('Bem vinda, ' + info.personagem);
+//console.log(`Bem vinda, ${info.personagem}`);
+//console.log(mensagem + info.personagem);
 
+//----------------------------------------------------------
+
+info['recorrente'] = 'Sim';
+//console.log(info);
+for (let chave in info){
+    console.log(chave);
+}
+
+for (let chave in info){
+    console.log(info[chave]);
+}
