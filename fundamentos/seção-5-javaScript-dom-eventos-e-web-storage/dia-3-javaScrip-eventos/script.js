a5c7f1d92473ab5d18cba7c5bc92fb1bbdb7b738
Adriana Chikuji <drikasousa92@gmail.com>
2022-10-01 15:35:56 -0300

    comecando o exercicio

--- fundamentos/seção-5-javaScript-dom-eventos-e-web-storage/dia-3-javaScrip-eventos/script.js
+++ fundamentos/seção-5-javaScript-dom-eventos-e-web-storage/dia-3-javaScrip-eventos/script.js
@@ -13,5 +13,31 @@ function createDaysOfTheWeek() {
   
   createDaysOfTheWeek();
   
-  // Escreva seu código abaixo.
+  const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
   
+  function daysMonth(){
+    let listDays = document.querySelector('#days');
+
+    for (let index = 0; index < decemberDaysList; index += 1){
+        let day = decemberDaysList[index];
+        let dayListItem = document.createElement('li');
+        dayListItem.innerHTML = day;
+
+        if (day === 24 || day === 31){
+            itemDay.className = 'Feriado';
+            listDays.appendChild(dayListItem);
+
+        } else if ( day === 4 || day === 11 || day === 18){
+            itemDay.className = 'Sexta-feira';
+            listDays.appendChild(dayListItem);
+
+        } else if (day === 25){
+            itemDay.className = 'feriado na Sexta feira';
+            listDays.appendChild(dayListItem);
+        } else {
+            dayListItem.className ='dia'
+            listDays.appendChild(dayListItem);
+        }
+    }
+  }
+  daysMonth();
