cc07272d69ed137e426bd8bfa7284e1f9f51c584
Adriana Chikuji <drikasousa92@gmail.com>
2022-10-03 21:45:56 -0300

    exercicio completo

--- fundamentos/seção-5-javaScript-dom-eventos-e-web-storage/dia-4-javaScrip-web-Storage/script.js
+++ fundamentos/seção-5-javaScript-dom-eventos-e-web-storage/dia-4-javaScrip-web-Storage/script.js
@@ -3,5 +3,24 @@ window.onload = function(){
   select.addEventListener('change',function(){
    let selecionado = select.selectedOptions[0];
     document.body.style.backgroundColor = select.value
+
+    localStorage.setItem('4-background-color', select.value);;
   })
+  let inputfontSize = document.querySelector('input[name="fonte-size');
+  inputfontSize.addEventListener('change', function(){
+    let p = document.querySelector('p');
+   p.style.fontSize = `${inputfontSize.value}px`;
+
+   localStorage.setItem('4-font-size',`${inputfontSize.value}px`);
+  })
+  
+  let salvabackground = localStorage.getItem('4-background-color');
+  document.body.style.backgroundColor = salvabackground;
+
+  let savefontSize = localStorage.getItem('4-font-size')
+  let p = document.querySelector('p');
+  p.style.fontSize = savefontSize;
+
+ 
 }
+
