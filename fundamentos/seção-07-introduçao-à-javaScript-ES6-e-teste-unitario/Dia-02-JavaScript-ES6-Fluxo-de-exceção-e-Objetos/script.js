a3f26245e0b12c6cbf7b6ba10e25a6500984ce75
Adriana Chikuji <drikasousa92@gmail.com>
2022-10-19 18:44:22 -0300

    atualizando

--- fundamentos/seção-07-introduçao-à-javaScript-ES6-e-teste-unitario/Dia-02-JavaScript-ES6-Fluxo-de-exceção-e-Objetos/script.js
+++ fundamentos/seção-07-introduçao-à-javaScript-ES6-e-teste-unitario/Dia-02-JavaScript-ES6-Fluxo-de-exceção-e-Objetos/script.js
@@ -49,3 +49,33 @@ const order = {
   
   orderModifier(order);
 
+//_____________________________________________________________________________________________
+ 
+    const lesson1 = {
+    materia: 'Matemática',
+    numeroEstudantes: 20,
+    professor: 'Maria Clara',
+    turno: 'manhã',
+  };
+  
+  const lesson2 = {
+    materia: 'História',
+    numeroEstudantes: 20,
+    professor: 'Carlos',
+  };
+  
+  const lesson3 = {
+    materia: 'Matemática',
+    numeroEstudantes: 10,
+    professor: 'Maria Clara',
+    turno: 'noite',
+  };
+  
+  const allLessons = Object.assign({}, { lesson1, lesson2, lesson3 });
+  // O método "Object.assing()" recebe dois parametros, um objeto, e o que queremos atribuir à ele. Se no primeiro parametro passarmos um objeto vazio ele criará um objeto novo com as carateristicas do segundo parâmetro.
+  
+  function getStudentsTotal(lessons){
+    return lessons
+  }
+ 
+  console.log(getStudentsTotal(allLessons))
