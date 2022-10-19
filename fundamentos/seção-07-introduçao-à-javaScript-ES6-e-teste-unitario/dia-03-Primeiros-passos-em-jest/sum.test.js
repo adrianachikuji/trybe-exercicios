ba3254d8f887d9f653e72e6810fe0f1b376c070a
Adriana Chikuji <drikasousa92@gmail.com>
2022-10-19 20:27:21 -0300

    exercicio 3

--- fundamentos/seção-07-introduçao-à-javaScript-ES6-e-teste-unitario/dia-03-Primeiros-passos-em-jest/sum.test.js
+++ fundamentos/seção-07-introduçao-à-javaScript-ES6-e-teste-unitario/dia-03-Primeiros-passos-em-jest/sum.test.js
@@ -1,12 +1,12 @@
 const sum = require('./sum.js');
 
-describe('testar a função sum'), () => {
+describe('testar a função sum', () => {
  it( 'deve retornar o resultado da soma', () => {
-    experct(sum (4, 5).toBe(9));
-    experct(sum(0, 0).toBe(0));
+    expect(sum(4, 5)).toBe(9);
+    expect(sum(0, 0)).toBe(0);
  });
  it('deve disparar um erro caso receba como parâmetro uma string', () => {
    expect(() => sum(4, '5')).toThrowError();
    expect(() => sum(4, '5')).toThrowError('parameters must be numbers');
  });
-}
+});
