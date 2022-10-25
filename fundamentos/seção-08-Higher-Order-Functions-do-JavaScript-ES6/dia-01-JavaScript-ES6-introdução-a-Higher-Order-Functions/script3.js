2d5be71d8ba09d199db3d90ee02cc97b16bfaa2a
Adriana Chikuji <drikasousa92@gmail.com>
2022-10-25 15:11:48 -0300

    add novo dia

--- fundamentos/seção-08-Higher-Order-Functions-do-JavaScript-ES6/dia-01-JavaScript-ES6-introdução-a-Higher-Order-Functions/script3.js
+++ fundamentos/seção-08-Higher-Order-Functions-do-JavaScript-ES6/dia-01-JavaScript-ES6-introdução-a-Higher-Order-Functions/script3.js
@@ -1,5 +1,3 @@
-const { Action } = require("argparse");
-
 const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
 const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
 const compararAnswers = (rightAnswers, studentAnswers) => {
